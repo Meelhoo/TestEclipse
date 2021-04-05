20efb72be8e129880696ff731bf49ace5518bcd0
emile <emile@LAPTOP-3N70F722.home>
2021-04-05 17:03:55 +0200

    Ajout d'une 3e ligne de texte

--- src/ClassTest.java
+++ src/ClassTest.java
@@ -4,6 +4,7 @@ public class ClassTest {
 	public static void main(String[] args) {
 		System.out.println("Hello les lolos");
 		System.out.println("Here we gogo");
+		System.out.println("Petite modif: ceci est une 3e ligne");
 
 	}
 
